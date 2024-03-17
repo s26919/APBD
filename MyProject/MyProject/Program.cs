0269af23acbfc0a28d3b765634c9b5eef3497ff1
s26919 <s26919@pjwstk.edu.pl>
2024-03-17 14:39:23 +0100

    add findmax fun

--- MyProject/MyProject/Program.cs
+++ MyProject/MyProject/Program.cs
@@ -12,6 +12,8 @@ namespace MyProject
             Console.WriteLine("Mod3");
             int[] fgh = {1, 2, 3, 4, 5, 6 };
             Console.WriteLine(CalculateAverage(fgh));
+            Console.WriteLine(FindMax(fgh));
+
             
 
         }
@@ -24,6 +26,18 @@ namespace MyProject
             }
 
             return (double)sum / numbers.Length;
+        }
+        public static int FindMax(int[] numbers)
+        {
+            int max = numbers[0]; 
+
+            foreach (int num in numbers)
+            {
+                if (num > max)
+                    max = num; 
+            }
+
+            return max;
         }
     }
 }
